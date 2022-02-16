951c30e652c4e196e0b697db64c6ab7846301577
Max Fortun <Max.Fortun@wsj.com>
2022-02-16 14:19:00 -0500

    Added

--- djBuildAndRelease.sh
+++ djBuildAndRelease.sh
@@ -7,5 +7,5 @@ $WD/build.sh
 url=https://artifactory-publish.prod.dowjones.io/artifactory/djsp-pubedit-release-local
 
 for artifact in xalan serializer; do
-	mvn deploy:deploy-file -DrepositoryId=artifactory -Durl=$url -Dpackaging=jar -DgroupId=com.dowjones.xalan -DartifactId=artifact -Dversion=2.7.2.1 -Dfile=$WD/build/$artifact.jar
+	mvn deploy:deploy-file -DrepositoryId=artifactory -Durl=$url -Dpackaging=jar -DgroupId=com.dowjones.xalan -DartifactId=$artifact -Dversion=2.7.2.1 -Dfile=$WD/build/$artifact.jar
 done
