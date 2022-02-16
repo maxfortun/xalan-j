253ada2488c16a4d359b2ef3d0d77b965a3f2709
Max Fortun <Max.Fortun@wsj.com>
2022-02-16 14:30:47 -0500

    Added

--- djBuildAndRelease.sh
+++ djBuildAndRelease.sh
@@ -1,10 +1,18 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 WD=$(dirname $0)
 
+url=$1
+if [ -z "$url" ]; then
+	echo "Usage: $0 <maven repo>"
+	echo " e.g.: $0 https://artifactory.domain.com/artifactory/repo-release-local"
+	exit 1
+fi
+
 $WD/build.sh
 
-url=https://artifactory-publish.prod.dowjones.io/artifactory/djsp-pubedit-release-local
+
+
 
 for artifact in xalan serializer; do
 	mvn deploy:deploy-file -DrepositoryId=artifactory -Durl=$url -Dpackaging=jar -DgroupId=com.dowjones.xalan -DartifactId=$artifact -Dversion=2.7.2.1 -Dfile=$WD/build/$artifact.jar
